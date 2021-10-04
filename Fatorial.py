bb11db52d2be0ac3e80880112557f68cbe4ea91b
yuri nunes <68084915+yuriney@users.noreply.github.com>
2021-10-04 15:20:02 -0300

    Add files via upload

---
+++ Fatorial.py
@@ -0,0 +1,9 @@
+num = int(input())
+i = 1
+fact = 1
+while i <= num:
+
+    fact *= i
+    i+=1
+print(f'numero = {num} fatorial = {fact}')
+print(2*'vezes')
---
+++ ImparEX.py
@@ -0,0 +1,7 @@
+x = int(input())
+if(x == 0):
+    print("X eh zero!")
+elif(x % 2) != 0:
+    print("X eh impar")
+else:
+    print("X eh par")
